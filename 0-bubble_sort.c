2d7e5a2962f3aac20d5fc6c8d285585fd2e1597a
Festus Omaku <fomaku3@gmail.com>
2023-02-19 10:13:30 +0100

    task bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ *bubble_sort - Sorts an array of integers in ascending order using
+ *buble sorts algorithm
+ *@array: the arrays of figures to be sorted
+ *@size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
