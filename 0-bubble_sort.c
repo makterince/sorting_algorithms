874ea8eb9620dff543150dc9b86ad007508cbc9d
Festus Omaku <fomaku3@gmail.com>
2023-02-19 10:20:56 +0100

    task bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-
-	if (array == NULL || size < 2)
-		return;
+	bool swapped;
+	
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,9 +22,12 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = true;
 				print_array(array, size);
 			}
 		}
+		if (swapped == false)
+			break;
 	}
 }
 
